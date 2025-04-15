97138da7eed9c2659bdfd53f2279b1e44845a625
Doug <douglas@macOs.local>
2025-04-15 19:43:28 -0300

    Creating component using cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component'
 import { HelloWorldComponent } from './helloworld/helloworld.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [HelloWorldComponent, MinhapaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
