de619d814dfa0aa521f0a997ab2baa98edc1c72e
Doug <douglas@macOs.local>
2025-04-15 19:20:34 -0300

    First commit and first component

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloworld/helloworld.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HelloWorldComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'conceitos-basicos';
+}
